153a6d90a6090c408bf79dcddb67e7973dd385f3
SaltyBuckets <49991711+SaltyBuckets@users.noreply.github.com>
2020-10-12 13:28:37 +0530

    add history support
    users can now see last 100 messages

--- server.js
+++ server.js
@@ -4,45 +4,46 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 let messages = [];
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+  res.sendFile('index.html');
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+  console.log('a user connected');
+  socket.emit('init', messages);
 
-    socket.on('chatMessage', (message) => {
-        if (message !== "") {
-            const date = getDateTime();
-            const msg = { message, date };
-            console.log(`${msg.date}:${msg.message}`);
-            io.emit("chatMessage", msg);
-            messages.push(msg)
-        }
-    });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 
-
+  socket.on('chatMessage', (message) => {
+    if (message !== '') {
+      const date = getDateTime();
+      const msg = { message, date };
+      // console.log(`${msg.date}:${msg.message}`);
+      io.emit('chatMessage', msg);
+      messages.push(msg);
+      if (messages.length >= 100) messages.pop();
+    
+    }
+  });
 });
 
-
-
-
 http.listen(port, () => {
-    console.log(`listening on *:${port}`);
+  console.log(`listening on *:${port}`);
 });
 
 function getDateTime() {
-    const today = new Date();
-    const date = `${today.getDate()}-${(today.getMonth() + 1)}-${today.getFullYear()}`;
-    const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
-    const dateTime = `${date} ${time}`
+  const today = new Date();
+  const date = `${today.getDate()}-${
+    today.getMonth() + 1
+  }-${today.getFullYear()}`;
+  const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+  const dateTime = `${date} ${time}`;
 
-    return dateTime;
+  return dateTime;
 }
