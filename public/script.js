c51c5aa5b9143360e6bb13d6b3d07ba94520e910
VibhorJaiswal <69683278+VibhorJaiswal@users.noreply.github.com>
2020-10-13 21:02:23 +0530

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,43 @@
+let ul = document.querySelector('#messages > ul');
+
+$(function () {
+    var socket = io();
+    $('#send').click(function (e) {
+        const message = $('#write-msg').val();
+        let msg = {
+            message: message,
+        };
+        socket.emit('chatMessage', msg);
+        $('#write-msg').val('');
+        return false;
+    });
+
+    socket.on('init', (data) => {
+        console.log(data);
+        data.forEach((msg) => {
+            addMessage(msg);
+        });
+    });
+
+    socket.on('chatMessage', function (msg) {
+        addMessage(msg);
+        console.log(msg);
+    });
+});
+
+function addMessage(msg) {
+    let li = document.createElement("li");
+    li.setAttribute("id", "msg");
+    let span = document.createElement("span");
+    span.setAttribute("id", "time");
+    span.innerText = `${msg.date}`;
+    li.innerText = `${msg.message}`;
+    li.appendChild(span);
+    ul.appendChild(li);
+    scrolly();
+}
+
+function scrolly() {
+    let element = document.getElementById("messages");
+    element.scrollTop = element.scrollHeight;
+}
