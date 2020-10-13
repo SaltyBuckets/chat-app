d28e491e291042fba438fa0927cabb588b258449
SaltyBuckets <49991711+SaltyBuckets@users.noreply.github.com>
2020-10-13 08:26:57 +0530

    add username support

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 let messages = [];
+let users = [];
 const port = process.env.PORT || 3000;
-
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -13,9 +13,19 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.emit('init', messages);
+  socket.on('auth', (user) => {
+    console.log(`${user.name} connected`);
+    const usr = {
+      name: user.name,
+      id: socket.id,
+    };
+    users.push(usr);
+    socket.emit('init', messages);
+  });
 
+  socket.on('connect', () => {
+    console.log(`a user connected`);
+  });
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
@@ -23,12 +33,17 @@ io.on('connection', (socket) => {
   socket.on('chatMessage', (message) => {
     if (message !== '') {
       const date = getDateTime();
-      const msg = { message, date };
-      // console.log(`${msg.date}:${msg.message}`);
+      const msg = {
+        message: message.message,
+        name: message.name,
+        date,
+      };
+
+      console.log(`${msg.name}:${msg.message}`);
+
       io.emit('chatMessage', msg);
       messages.push(msg);
-      if (messages.length >= 100) messages.pop();
-    
+      if (messages.length >= 300) messages.pop();
     }
   });
 });
